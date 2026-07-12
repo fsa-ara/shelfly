bfc8842d682e562080346b73caf889b9514b8e02
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-07-12 21:12:37 +0200

    feat(auth): implement user logout

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller
+{
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
+}
