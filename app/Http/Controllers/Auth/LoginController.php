301144f8a0f642d59a3f356b6763281139ad6e45
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-07-12 21:08:44 +0200

    feat(auth): implement user authentication

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+use Illuminate\View\View;
+
+class LoginController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => [
+                'required',
+                Rule::email(),
+            ],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials, $request->boolean('remember-me'))) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid email address or password',
+        ])->onlyInput('email');
+    }
+}
