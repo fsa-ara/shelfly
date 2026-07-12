6aec2872cab9d30ee308bd6d7312783a886fadc7
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-07-12 21:19:39 +0200

    feat(auth): add authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->name('dashboard');
+
+Route::prefix('account')
+    ->group(function () {
+        Route::get('sign-in', [LoginController::class, 'create'])->name('login');
+        Route::post('sign-in', [LoginController::class, 'store'])->name('login.store');
+        Route::get('sign-up', [RegisterController::class, 'create'])->name('register');
+        Route::post('sign-up', [RegisterController::class, 'store'])->name('register.store');
+        Route::post('logout', [LogoutController::class, 'destroy'])->name('logout');
+    });
