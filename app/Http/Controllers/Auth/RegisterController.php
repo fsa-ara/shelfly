9aa10e5d41d1396907330ace34209483ff43af39
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-07-13 13:56:50 +0200

    feat(auth): implement email verification

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,15 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use App\Services\RegisterService;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
-use Illuminate\Validation\Rule;
-use Illuminate\Validation\Rules\Password;
 use Illuminate\View\View;
 
 class RegisterController extends Controller
@@ -27,42 +23,16 @@ class RegisterController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
+    public function store(RegisterRequest $request, RegisterService $service): RedirectResponse
     {
-        $credentials = $request->validate([
-            'email' => [
-                'required',
-                Rule::email()
-                    ->preventSpoofing()
-                    ->rfcCompliant(strict: false)
-                    ->validateMxRecord(),
-            ],
-            'password' => [
-                'confirmed',
-                'required',
-                Password::min(12)
-                    ->letters()
-                    ->mixedCase()
-                    ->numbers()
-                    ->symbols()
-                    ->uncompromised(),
-            ],
-        ]);
-
-        $user = DB::transaction(function () use ($credentials) {
-            $user = User::create([
-                'uuid' => Str::uuid(),
-                'email' => $credentials['email'],
-                'password' => Hash::make($credentials['password']),
-            ]);
-
-            return $user;
-        });
+        $user = $service->handle($request->validated());
 
         Auth::login($user);
 
+        event(new Registered($user));
+
         $request->session()->regenerate();
 
-        return redirect()->route('dashboard');
+        return redirect()->route('verification.notice');
     }
 }
