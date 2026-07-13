1504d36cac0f79d60593da03d3b0be4310516577
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-07-13 13:58:30 +0200

    feat(auth): add email verification routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerifyController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
@@ -11,9 +12,24 @@ Route::get('/', function () {
 
 Route::prefix('account')
     ->group(function () {
-        Route::get('sign-in', [LoginController::class, 'create'])->name('login');
-        Route::post('sign-in', [LoginController::class, 'store'])->name('login.store');
-        Route::get('sign-up', [RegisterController::class, 'create'])->name('register');
-        Route::post('sign-up', [RegisterController::class, 'store'])->name('register.store');
-        Route::post('logout', [LogoutController::class, 'destroy'])->name('logout');
+        Route::get('sign-in', [LoginController::class, 'create'])
+            ->name('login');
+        Route::post('sign-in', [LoginController::class, 'store'])
+            ->name('login.store');
+        Route::get('sign-up', [RegisterController::class, 'create'])
+            ->name('register');
+        Route::post('sign-up', [RegisterController::class, 'store'])
+            ->name('register.store');
+        Route::post('logout', [LogoutController::class, 'destroy'])
+            ->name('logout');
+
+        Route::get('email/verify', [EmailVerifyController::class, 'create'])
+            ->middleware(['auth'])
+            ->name('verification.notice');
+        Route::get('/email/verify/{id}/{hash}', [EmailVerifyController::class, 'handle'])
+            ->middleware(['auth', 'signed'])
+            ->name('verification.verify');
+        Route::post('/email/verify/notification', [EmailVerifyController::class, 'store'])
+            ->middleware(['auth', 'throttle:5,1'])
+            ->name('verification.send');
     });
