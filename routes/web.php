908f29b3d27b196bdd3a95640d3b6ae4305b705b
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-09-03 21:58:02 +0200

    feat(routes): adds sign in route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Pages\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('account')
+    ->group(function () {
+        Route::get('sign-in', [LoginController::class, 'index'])
+            ->middleware('guest')
+            ->name('auth.login');
+        Route::post('sign-in', [LoginController::class, 'authenticate']);
+    });
