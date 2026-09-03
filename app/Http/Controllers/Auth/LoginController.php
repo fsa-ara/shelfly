daefa9111443bbba85af980096876d61a03237d2
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-09-03 21:51:37 +0200

    feat(authentication): adds LoginController and LoginRequest

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('pages.login');
+    }
+
+    public function authenticate(LoginRequest $request): RedirectResponse
+    {
+        $credentials = $request->validated();
+
+        if (Auth::attempt($credentials, $request->boolean('remember-me'))) {
+            $request->session()->regenerate();
+
+            return redirect()->route('home');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid email or password.'
+        ])->onlyInput('email');
+    }
+}
