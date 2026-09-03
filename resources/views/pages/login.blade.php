6d77a48780b5958edcdb503a37de772bd1467e34
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-09-03 21:57:42 +0200

    feat(views): adds login view

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,56 @@
+<x-app>
+    @php
+        $hasError = $errors->has('email');
+    @endphp
+    <div @class([
+        'bg-white dark:bg-black',
+        'col-span-full -mx-4 overflow-y-scroll',
+    ])>
+        <div @class(['px-4 py-16 mx-auto', 'sm:max-w-sm', 'lg:max-x-lg lg:mt-12'])>
+            <x-heading level="1"
+                       content="Sign In" />
+            <p @class(['my-8 text-center'])>Sign in to your account to access all services.</p>
+            <x-form id="sign-in-form"
+                    action="{{ route('auth.login') }}"
+                    method="post">
+                <div @class(['flex flex-col gap-2'])>
+                    <x-form.input id="sign-in-email-field"
+                                  name="email"
+                                  type="email"
+                                  value="{{ old('email') }}"
+                                  autocomplete="username"
+                                  required
+                                  label="Email"
+                                  :hasError="$hasError" />
+                    <x-form.input id="sign-in-password-field"
+                                  name="password"
+                                  type="password"
+                                  required
+                                  label="Password"
+                                  :hasError="$hasError" />
+                    <div @class(['h-16'])>
+                        <x-form.error type="email" />
+                    </div>
+                </div>
+                <div @class(['flex justify-center mb-8'])>
+                    <x-form.input id="sign-in-remember-me-checkbox"
+                                  name="remember-me"
+                                  type="checkbox"
+                                  label="Remember me" />
+                </div>
+                <x-form.button form="sign-in-form"
+                               type="submit"
+                               text="Sign In" />
+            </x-form>
+            <div @class(['my-8 text-center'])>
+                <x-nav.link href=""
+                            text="Forgot your password?" />
+            </div>
+            <div @class(['flex flex-wrap gap-x-2 justify-center'])>
+                <p>Don't have a {{ config('app.name') }} account?</p>
+                <x-nav.link href=""
+                            text="Create a {{ config('app.name') }} account" />
+            </div>
+        </div>
+    </div>
+</x-app>
