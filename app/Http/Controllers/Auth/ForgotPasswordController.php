8bcf08e03bb2755d9707a56836c9489a805f8ff8
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-09-07 13:35:25 +0200

    feat(authentication): adds ForgotPasswordController and ForgotPasswordRequest

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ForgotPasswordRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Password;
+use Illuminate\View\View;
+
+class ForgotPasswordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('pages.forgot-password');
+    }
+
+    public function send(ForgotPasswordRequest $request): RedirectResponse
+    {
+        $credentials = $request->validated();
+
+        $status = Password::sendResetLink($credentials);
+
+        if ($status === Password::ResetLinkSent) {
+            $request->session()->put('forgot_password_email', $credentials['email']);
+
+            return back()->with([
+                'status' => __($status),
+            ]);
+        }
+
+        return back()->withErrors([
+            'email' => __($status),
+        ]);
+    }
+}
