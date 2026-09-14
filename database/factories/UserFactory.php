ef6d8ef57aaa4b66ca9279e83f1eb3bda62df550
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-09-14 13:31:17 +0200

    feat(user): modify the model and implement the factory, migration and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,11 +25,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'uuid' => fake()->unique()->uuid(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'remember_token' => Str::random(64),
+            'email_verified_at' => now(),
         ];
     }
 
@@ -38,7 +38,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
