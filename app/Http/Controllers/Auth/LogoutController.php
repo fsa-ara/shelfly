11312d9435a8b74e520e92af0f0c5903476514c9
Florent SARTOUT <129736879+fsa-ara@users.noreply.github.com>
2026-09-14 14:41:08 +0200

    feat(auth): add the LogoutController and implement the route to logout

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller
+{
+    public function deauthenticate(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login')->with([
+            'status' => 'You are logged out!',
+        ]);
+    }
+}
